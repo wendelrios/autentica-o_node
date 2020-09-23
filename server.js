b3c72d07187e09a552a1eeb4a603245838285808
Wendel <wendelrioss@hotmail.com>
2020-09-23 16:41:22 -0300

    commit queries changes

---
+++ server.js
@@ -0,0 +1,153 @@
+const express = require("express");
+const { pool } = require("./dbConfig");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+require("dotenv").config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const initializePassport = require("./passportConfig");
+
+initializePassport(passport);
+
+// Middleware
+
+// Parses details from a form
+app.use(express.static(__dirname, +'./public'))
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+
+app.use(
+  session({
+    // Key we want to keep secret which will encrypt all of our information
+    secret: process.env.SESSION_SECRET,
+    // Should we resave our session variables if nothing has changes which we dont
+    resave: false,
+    // Save empty value if there is no vaue which we do not want to do
+    saveUninitialized: false
+  })
+);
+// Funtion inside passport which initializes passport
+app.use(passport.initialize());
+// Store our variables to be persisted across the whole session. Works with app.use(Session) above
+app.use(passport.session());
+app.use(flash());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/register", checkAuthenticated, (req, res) => {
+  res.render("register.ejs");
+});
+
+// app.get("/users/login", checkAuthenticated, (req, res) => {
+//   // flash sets a messages variable. passport sets the error message
+//   console.log(req.session.flash.error);
+//   res.render("login.ejs");
+// });
+
+app.get("/users/home", checkNotAuthenticated, (req, res) => {
+  console.log(req.isAuthenticated());
+  res.render("home");
+});
+
+app.get("/users/logout", (req, res) => {
+  req.logout();
+  res.render("index", { message: "You have logged out successfully" });
+});
+
+app.post("/register", async (req, res) => {
+  let { name, email, password, password2 } = req.body;
+
+  let errors = [];
+
+  console.log({
+    name,
+    email,
+    password,
+    password2
+  });
+
+  if (!name || !email || !password || !password2) {
+    errors.push({ message: "Please enter all fields" });
+  }
+
+  if (password < 6) {
+    errors.push({ message: "Password must be a least 6 characters long" });
+  }
+
+  if (password !== password2) {
+    errors.push({ message: "Passwords do not match" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", { errors, name, email, password, password2 });
+  } else {
+    hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+    // Validation passed
+    pool.query(
+      `SELECT * FROM users
+        WHERE email = $1`,
+      [email],
+      (err, results) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(results.rows);
+
+        if (results.rows.length > 0) {
+          return res.render("register", {
+            message: "Email already registered"
+          });
+        } else {
+          pool.query(
+            `INSERT INTO users (nome, email, senha)
+                VALUES ($1, $2, $3)
+                RETURNING id, senha`,
+            [name, email, hashedPassword],
+            (err, results) => {
+              if (err) {
+                throw err;
+              }
+              console.log(results.rows);
+              req.flash("success_msg", "You are now registered. Please log in");
+              res.redirect("/");
+            }
+          );
+        }
+      }
+    );
+  }
+});
+
+app.post(
+  "/",
+  passport.authenticate("local", {
+    successRedirect: "/users/home",
+    failureRedirect: "/",
+    failureFlash: true
+  })
+);
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/home");
+  }
+  next();
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
